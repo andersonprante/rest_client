d81701dee4eb1e9e55b0f8497001b8dadcc76687
Anderson <anderson.prante@gmail.com>
2022-10-29 20:34:20 -0300

    form ok

---
+++ app.js
@@ -0,0 +1,13 @@
+const input = document.querySelector('#url')
+const response = document.querySelector('#response')
+
+const makeRequest = async () => {
+    response.innerHTML = ''
+    url = input.value
+    const options = {
+        mode: 'cors',
+    }
+    const result = await fetch(url, options).then(response => response.text())
+    response.innerHTML = result
+}
+

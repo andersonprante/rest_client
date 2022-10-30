0abd3ad45e3e89dad49bc277866f35ff47af6900
Anderson <anderson.prante@gmail.com>
2022-10-30 11:31:03 -0300

    pwa

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+        navigator.serviceWorker
+            .register("/sw.js")
+            .then(res => console.log("service worker registered"))
+            .catch(err => console.log("service worker not registered", err))
+    })
+}
+
+
 const input = document.querySelector('#url')
 const response = document.querySelector('#response')
 
